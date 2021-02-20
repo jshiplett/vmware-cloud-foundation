ff642b73df3e92534af5b8d5e03b477c51a407a0
Jason Shiplett <jason@shiplett.org>
2021-02-20 15:29:28 -0600

    Add files via upload

---
+++ remove-edge-cluster-vcf.ps1
@@ -0,0 +1,36 @@
+$guestUser = "root"
+$guestPassword = 'VMw@re1!'
+$guestVm = "sfo-vcf01"
+$vcenterFqdn = "sfo-m01-vc01.sfo.rainpole.io"
+$vcenterUser = "administrator@vsphere.local"
+$vcenterPassword = "VMw@re1!"
+$edgeClusterCleanerUrl = "https://kb.vmware.com/sfc/servlet.shepherd/version/download/0685G00000NHZoBQAX"
+$edgeClusterDestination = "/root/edge_cluster_cleaner_0.18.tar.gz"
+$edgeClusterName = "sfo-w01-ec01"
+
+$removeEdgeCluster = @"
+tar -zxvf $edgeClusterDestination
+touch /root/.sddc_edge_removal_warning_accepted
+/root/cleanup/remove_edge_cluster.sh --cluster $edgeClusterName --user $vcenterUser --password $vcenterPassword--skip-warning
+"@
+
+$viserver = Connect-VIServer -Server $vcenterFqdn -User $vcenterUser -Password $vcenterPassword -Force
+
+try {
+    $tryDownloadSddcManager = Invoke-VMScript -VM $guestVm -ScriptType Bash -ScriptText "wget -O $edgeClusterDestination $edgeClusterCleanerUrl" -GuestUser $guestUser -GuestPassword $guestPassword
+} catch {
+    Write-Error $_.Exception.InnerException
+}
+
+$finished = ($tryDownloadSddcManager.ToString() -Split '\n' | Select-Object -Last 3) | Where-Object {$_ -match "[6678986/6678986]"}
+
+if ($finished){
+    try {
+        $tryRemoveEdgeCluster = Invoke-VMScript -VM $guestVm -ScriptType Bash -ScriptText $removeEdgeCluster -GuestUser $guestUser -GuestPassword $guestPassword
+        $tryRemoveEdgeCluster
+    } catch {
+        Write-Error $_.Exception.InnerException
+    }
+}
+
+Disconnect-VIServer * -Confirm:$false
