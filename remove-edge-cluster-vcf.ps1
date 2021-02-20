417bcd5bc7a46052033f59e4bf50eed7758b7580
Jason Shiplett <jason@shiplett.org>
2021-02-20 15:30:24 -0600

    Update remove-edge-cluster-vcf.ps1

--- remove-edge-cluster-vcf.ps1
+++ remove-edge-cluster-vcf.ps1
@@ -1,9 +1,9 @@
 $guestUser = "root"
-$guestPassword = 'VMw@re1!'
+$guestPassword = '<password>'
 $guestVm = "sfo-vcf01"
 $vcenterFqdn = "sfo-m01-vc01.sfo.rainpole.io"
 $vcenterUser = "administrator@vsphere.local"
-$vcenterPassword = "VMw@re1!"
+$vcenterPassword = '<password>'
 $edgeClusterCleanerUrl = "https://kb.vmware.com/sfc/servlet.shepherd/version/download/0685G00000NHZoBQAX"
 $edgeClusterDestination = "/root/edge_cluster_cleaner_0.18.tar.gz"
 $edgeClusterName = "sfo-w01-ec01"
